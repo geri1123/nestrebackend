f03693017bacc5ba05d97b153f5628a3eff5477d
geri1123 <geriscelmeta1@icloud.com>
2026-02-27 18:36:54 +0100

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 export default tseslint.config(
   {
@@ -20,7 +21,7 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: dirname(fileURLToPath(import.meta.url)),
       },
     },
   },
