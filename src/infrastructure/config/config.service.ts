807bcffcd60528001efa8fca5820914e9a41ea9f
geri1123 <geriscelmeta1@icloud.com>
2026-06-02 20:47:22 +0200

    config paysera

--- src/infrastructure/config/config.service.ts
+++ src/infrastructure/config/config.service.ts
@@ -165,7 +165,7 @@ get payseraSignPassword(): string {
 get payseraCallbackUrl(): string {
   return this.configService.get<string>(
     'PAYSERA_CALLBACK_URL',
-    `https://pronasmart.onrender.com/wallet/webhooks/paysera`,
+    `${this.clientBaseUrl}/wallet/webhooks/paysera`,
   );
 }
  
